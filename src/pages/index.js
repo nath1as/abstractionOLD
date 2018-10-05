48884ea6f4ab1b333e33e44bbc78df7c54207997
nathias <n@th1.as>
2018-10-05 20:04:43 +0200

    refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,61 +40,75 @@ class BlogIndex extends React.Component {
     const siteDescription = get( this, 'props.data.site.siteMetadata.description')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
     const decide = this.state.decide;
-    
+
     if (decide === 'theory') {
-    return (
-    <div>
-        <Menu />
-      <Layout location={this.props.location}>
-        <Helmet htmlAttributes={{ lang: 'en' }} meta={[{ name: 'description', content: siteDescription }]} title={siteTitle} category={siteCategory} tags={siteTags} />
-        {posts.filter(({node}) => get(node, 'frontmatter.category') === 'theory' ).map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          const category = get(node, 'frontmatter.category') || node.fields.slug
-          const tags = get(node, 'frontmatter.tags') || node.fields.slug
-          return (
-                <Link to={node.fields.slug}>
-                  <div key={node.fields.slug}>
-                    <h4 className="category">{category}</h4>
-                    <h3 className="postTitle">{title}</h3>
-                    <p className="postContent" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                    <div className="datewrap">
-                      <h4 className="tags-wrap">{Object.values(tags).map( tag => <p className="tags">#{tag} </p> )}</h4>
-                      <h4 className="date">{node.frontmatter.date}</h4>
-                  </div>
-                  </div>
-                </Link>
-          )
-        })}
-      </Layout>
-    </div>
-    )
+      return (
+        <div>
+            <Menu />
+          <Layout location={this.props.location}>
+            <Helmet
+              htmlAttributes={{ lang: 'en' }}
+              meta={[{ name: 'description', content: siteDescription }]}
+              title={siteTitle}
+              category={siteCategory}
+              tags={siteTags} />
+                {posts.filter(({node}) =>
+                  get(node, 'frontmatter.category') === 'theory' ).map(({ node }) => {
+                    const title = get(node, 'frontmatter.title') || node.fields.slug
+                    const category = get(node, 'frontmatter.category') || node.fields.slug
+                    const tags = get(node, 'frontmatter.tags') || node.fields.slug
+                    return (
+                          <Link to={ node.fields.slug }>
+                            <div key={ node.fields.slug }>
+                              <h4 className="category">{ category }</h4>
+                              <h3 className="postTitle">{ title }</h3>
+                              <p className="postContent" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                              <div className="datewrap">
+                                <h4 className="tags-wrap">{ Object.values(tags).map( tag => <p className="tags">#{tag} </p> ) }</h4>
+                                <h4 className="date">{ node.frontmatter.date }</h4>
+                            </div>
+                            </div>
+                          </Link>
+                    )
+                 })
+                }
+          </Layout>
+        </div>
+      )
     } else if (decide === 'praxis') {
-    return (
-    <div>
-      <Menu />
-      <Layout location={this.props.location}>
-        <Helmet htmlAttributes={{ lang: 'en' }} meta={[{ name: 'description', content: siteDescription }]} title={siteTitle} category={siteCategory} tags={siteTags} />
-        {posts.filter(({node}) => get(node, 'frontmatter.category') === 'praxis' ).map(({ node }) => {
-          const title = get(node, 'frontmatter.title') || node.fields.slug
-          const category = get(node, 'frontmatter.category') || node.fields.slug
-          const tags = get(node, 'frontmatter.tags') || node.fields.slug
-          return (
-                <Link to={node.fields.slug}>
-                  <div key={node.fields.slug}>
-                    <h4 className="category">{category}</h4>
-                    <h3 className="postTitle">{title}</h3>
-                    <p className="postContent" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                    <div className="datewrap">
-                      <h4 className="tags-wrap">{Object.values(tags).map( tag => <p className="tags">#{tag} </p> )}</h4>
-                      <h4 className="date">{node.frontmatter.date}</h4>
-                  </div>
-                  </div>
-                </Link>
-          )
-        })}
-      </Layout>
-    </div>
-    )
+      return (
+        <div>
+          <Menu />
+          <Layout location={this.props.location}>
+            <Helmet
+              htmlAttributes={{ lang: 'en' }}
+              meta={[{ name: 'description', content: siteDescription }]}
+              title={siteTitle}
+              category={siteCategory}
+              tags={siteTags} />
+                {posts.filter(({node}) =>
+                  get(node, 'frontmatter.category') === 'praxis' ).map(({ node }) => {
+                    const title = get(node, 'frontmatter.title') || node.fields.slug
+                    const category = get(node, 'frontmatter.category') || node.fields.slug
+                    const tags = get(node, 'frontmatter.tags') || node.fields.slug
+                    return (
+                          <Link to={node.fields.slug}>
+                            <div key={node.fields.slug}>
+                              <h4 className="category">{category}</h4>
+                              <h3 className="postTitle">{title}</h3>
+                              <p className="postContent" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                              <div className="datewrap">
+                                <h4 className="tags-wrap">{Object.values(tags).map( tag => <p className="tags">#{tag} </p> )}</h4>
+                                <h4 className="date">{node.frontmatter.date}</h4>
+                            </div>
+                            </div>
+                          </Link>
+                    )
+                 })
+                }
+          </Layout>
+        </div>
+      )
     } else {
 
     return (
